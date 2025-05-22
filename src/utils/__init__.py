e5878f369c614d6eaa046028e8179bf826526a4d
wwan0220 <wwan0220@student.monash.edu>
2025-05-23 02:19:05 +0800

    超级用户

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-工具模块
+工具模块初始化文件
 """
 
+from src.utils.id_generator import IdGenerator
 from src.utils.file_util import FileUtil
 from src.utils.date_util import DateUtil
 
